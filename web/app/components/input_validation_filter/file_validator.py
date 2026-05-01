5b3cebbc8ba3f5af64da304201b2c1f50531217e
Rodrigo Miguel Santos Rodrigues <98055826+rmsr2004@users.noreply.github.com>
2026-05-01 10:10:04 +0100

    [SR-04_AD-03a] Feat: Upload file validation - file extension and MIME type checking (#103)
    * chore: updates dependencies
    
    * feat: validate mime type from uploaded file
    
    * feat: validates file extensions on upload
    
    * test: implements the tests for SR-04 and AD-03a

---
+++ web/app/components/input_validation_filter/file_validator.py
@@ -0,0 +1,51 @@
+import magic
+from werkzeug.datastructures import FileStorage
+from app.shared.result.Result import Result, Error
+
+ALLOWED_EXTENSIONS = {
+    'txt', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'png', 'jpg', 'jpeg'
+}
+
+ALLOWED_MIME_TYPES = {
+    'text/plain',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'application/vnd.ms-powerpoint',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'image/png',
+    'image/jpeg'
+}
+
+def validate_file(file: FileStorage):
+    if not file or not file.filename:
+        return Result.fail(Error("No file provided", 400))
+    
+    is_extension_valid = _validate_file_extension(file)
+    if not is_extension_valid:
+        return Result.fail(Error("Invalid file extension", 400))
+    
+    is_mime_type_valid = _validate_mime_type(file)
+    if not is_mime_type_valid:
+        return Result.fail(Error("Invalid MIME type", 400))
+    
+    return Result.ok(value=file)
+
+def _validate_mime_type(file: FileStorage) -> bool:
+    header = file.stream.read(1024)  # Read the first 1024 bytes to determine MIME type
+    file.stream.seek(0)  # Reset the stream position after reading
+    
+    mime_type = magic.from_buffer(header, mime=True)
+    
+    return mime_type in ALLOWED_MIME_TYPES
+
+def _validate_file_extension(file: FileStorage) -> bool:
+    filename = file.filename
+    extension = _extract_extension(filename)    
+    return extension in ALLOWED_EXTENSIONS
+
+def _extract_extension(filename: str) -> str:
+    if '.' not in filename:
+        return ''
+    
+    return filename.rsplit('.', 1)[1].lower()
