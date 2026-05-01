5a6f9bd07279d7203ba5737d8476ee11a6d93a8f
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-05-01 10:05:38 +0100

    feat: validates file extensions on upload

--- web/app/components/input_validation_filter/file_validator.py
+++ web/app/components/input_validation_filter/file_validator.py
@@ -2,6 +2,10 @@ import magic
 from werkzeug.datastructures import FileStorage
 from app.shared.result.Result import Result, Error
 
+ALLOWED_EXTENSIONS = {
+    'txt', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'png', 'jpg', 'jpeg'
+}
+
 ALLOWED_MIME_TYPES = {
     'text/plain',
     'application/pdf',
@@ -17,8 +21,12 @@ def validate_file(file: FileStorage):
     if not file or not file.filename:
         return Result.fail(Error("No file provided", 400))
     
-    result = _validate_mime_type(file)
-    if not result:
+    is_extension_valid = _validate_file_extension(file)
+    if not is_extension_valid:
+        return Result.fail(Error("Invalid file extension", 400))
+    
+    is_mime_type_valid = _validate_mime_type(file)
+    if not is_mime_type_valid:
         return Result.fail(Error("Invalid MIME type", 400))
     
     return Result.ok(value=file)
@@ -30,3 +38,14 @@ def _validate_mime_type(file: FileStorage) -> bool:
     mime_type = magic.from_buffer(header, mime=True)
     
     return mime_type in ALLOWED_MIME_TYPES
+
+def _validate_file_extension(file: FileStorage) -> bool:
+    filename = file.filename
+    extension = _extract_extension(filename)    
+    return extension in ALLOWED_EXTENSIONS
+
+def _extract_extension(filename: str) -> str:
+    if '.' not in filename:
+        return ''
+    
+    return filename.rsplit('.', 1)[1].lower()
